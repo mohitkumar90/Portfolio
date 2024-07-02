81805ae3081a69bf8ea7dfcbc50ee01c350a3135
Himanshu Sharma <mine@Himanshus-MacBook-Air.local>
2024-07-02 23:50:10 +0530

    Skills section

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,7 @@ const CustomLink = ({href, title, className=""})=>{
     <Link href={href} className={`${className} relative`}>
       {title}
       <span className={`
-      h-[2px] inline-block bg-dark absolute left-0 -bottom-0.5 left-0 group-hover:w-full transition-[width] ease duration-300 
+      h-[2px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 
       ${router.asPath === href ? 'w-full' : 'w-0'}
       `}>&nbsp;</span>
       </Link>
