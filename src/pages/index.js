bb5651bdbad56fff7975dc02d18bad6529070cad
Himanshu Sharma <mine@Himanshus-MacBook-Air.local>
2024-07-05 01:16:32 +0530

    home page responsive and mobile nav bar changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,33 +17,35 @@ export default function Home() {
      
       </Head>
       <main className='flex items-center text-dark w-full min-h-screen dark:text-light'>
-        <Layout>
-          <div className='flex items-center justify-between w-full'>
-            <div className='w-1/2'>
-            <Image src={profilePic} alt="MohitKumar" className="h-auto" 
+        <Layout className="pt-0 md:p-16 sm:pt-8">
+          <div className='flex items-center justify-between w-full lg:flex-col'>
+            <div className='w-1/2 md:w-full'>
+            <Image src={profilePic} alt="MohitKumar" className="w-full h-auto lg:hidden md:inline-block md:w-full " 
             priority
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
             />
             </div>
-            <div className='w-1/2 flex flex-col items-center self-center'>
-              <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl text-left'/>
-              <p className='my-4 text-base font-medium'>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web application. 
+            <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
+              <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl text-left 
+              xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl
+              '/>
+              <p className='my-4 text-base font-medium md:text-sm sm:text-xs'>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web application. 
               Explore my latest projects and articles, showcasing my expertise in React.js and web development.
               </p>
-              <div className='flex items-center self-start mt-2'>
+              <div className='flex items-center self-start mt-2 lg:self-center'>
                 <Link 
                 target={"_blank"}
                 className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
                  hover:bg-light hover:text-dark border-solid border-2 border-transparent 
                  hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
-                 hover:dark:border-light' 
+                 hover:dark:border-light md:p-2 md:px-4 md:text-base' 
                  download={true}
                  href="/dummy.pdf"
                  >
                   Resume <LinkArrow className={"w-6 ml-1"}></LinkArrow>
                  </Link>
                 <Link href={"mailto:mk9729708377@gmail.com"} target={"_blank"}
-                className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light'
+                className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base'
                 >
                   Contact
                   </Link>
@@ -52,7 +54,7 @@ export default function Home() {
           </div>
         </Layout>
         <HireMe />
-        <div className='absolute right-8 bottom-8 inline-block w-24'>
+        <div className='absolute right-8 bottom-8 inline-block w-24 lg:hidden md-hidden sm:hidden'>
         <Image src={lightBulb} alt="mk" className='w-full h-auto' />
 
         </div>
